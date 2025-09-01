fc2991487d37e90eb9dbab6977621b624ee97d12
Abhishek Mishra <abhisheknidhan9@gmail.com>
2025-09-01 12:15:31 +0530

    Add files via upload

---
+++ Assigment 3 ii.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Square root of {num} = {square_root}")
+print(f"Natural log of {num} = {log_value}")
+print(f"Sine of {num} (radians) = {sine_value}")
